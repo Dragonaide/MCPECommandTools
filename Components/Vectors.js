d44190ec315245f7d00d33f58480abf81d69f251
Dragonaide <55767582+Dragonaide@users.noreply.github.com>
2019-11-10 10:57:21 -0500

    Create Vectors.js

---
+++ Components/Vectors.js
@@ -0,0 +1,15 @@
+export function Vector3(x, y, z) {
+  x = Number();
+  y = Number();
+  z = Number();
+  // Check if vector properties are numbers
+  if (x !== Number()) {
+    console.error("Vector3 properties can only be numbers.");
+  }
+  if (y !== Number()) {
+    console.error("Vector3 properties can only be numbers.");
+  }
+  if (z !== Number()) {
+    console.error("Vector3 properties can only be numbers.");
+  }
+}
